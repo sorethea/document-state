e1b85e832bf0c30da75f3ee60a076622d0887a37
Sorethea OK <sorethea@gmail.com>
2022-11-17 09:53:38 +0700

    Document State Trait

--- src/Traits/DocumentStateTrait.php
+++ src/Traits/DocumentStateTrait.php
@@ -2,6 +2,7 @@
 
 namespace Sorethea\DocumentState\Traits;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Sorethea\DocumentState\Models\DocumentState;
 
@@ -12,8 +13,8 @@ trait DocumentStateTrait
         return $this->morphMany(DocumentState::class,"document");
     }
 
-    protected function isActive():\Attribute{
-        return new \Attribute(
+    protected function isActive():Attribute{
+        return new Attribute(
             function (){
                 $documentState = $this->states()->where("active",true)->first();
                 if($documentState->state<2) return true;
