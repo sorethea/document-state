7c4fd3daf5dbd2fd80629d0b9bc503a47aa29c9c
Sorethea OK <sorethea@gmail.com>
2022-11-17 09:12:56 +0700

    Document State Trait

--- src/Traits/DocumentStateTrait.php
+++ src/Traits/DocumentStateTrait.php
@@ -7,7 +7,14 @@ use Sorethea\DocumentState\Models\DocumentState;
 
 trait DocumentStateTrait
 {
+    protected $appends = ["is_active"];
     public function states(): MorphMany{
         return $this->morphMany(DocumentState::class,"document");
+    }
+
+    protected function isActive():\Attribute{
+        return new \Attribute(
+            fn()=>true
+        );
     }
 }
